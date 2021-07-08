b225c0f0b42d7d22006fc8326c578dc75ee89b0e
Nathan Stahlman <nathanstahlman99@gmail.com>
2021-07-08 17:27:06 -0500

    Lay out the data structure, and start forming questions.

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,8 +1,8 @@
 <template>
-  <img class="w-12 h-12 top-3 left-1 absolute" src="../assets/NathanD_Logo.png"/>
+  <img class="w-12 h-12 top-1 left-1 absolute" src="../assets/NathanD_Logo.png"/>
   <div class="px-12">
-    <h3 v-if="questionIndex == 0" class="font-bold text-lg">What programming language should you start with?</h3>
-    <Question :question="{text: 'This is not even a question??'}"/>
+    <h3 v-if="questionIndex == null" class="font-bold text-xl">What programming language should you start with?</h3>
+    <Question text="This is not even a question??"/>
   </div>
 </template>
 
@@ -16,25 +16,7 @@ export default {
   },
   data() {
     return {
-      questionIndex: 1,
-      paths: [
-        'Web Developer',
-        'Software Engineer',
-        'Game Developer',
-        'Mobile Developer',
-        'Machine learning / AI Engineer',
-        'Database Manager'
-      ],
-      languages: [
-        'JavaScript',
-        'Python',
-        'Ruby',
-        'C',
-        'C++',
-        'Java',
-        'Flutter',
-        'SQL',
-      ],
+      questionIndex: null,
     }
   }
   
