a2fc8f1d98e7966b6a844e5baff2943abba49647
Nathan Stahlman <nathanstahlman99@gmail.com>
2021-07-08 17:45:08 -0500

    Add logic to display the questions on the quiz page.

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,13 +1,19 @@
 <template>
   <img class="w-12 h-12 top-1 left-1 absolute" src="../assets/NathanD_Logo.png"/>
   <div class="px-12">
-    <h3 v-if="questionIndex == null" class="font-bold text-xl">What programming language should you start with?</h3>
-    <Question text="This is not even a question??"/>
+    <div v-if="questionIndex == null">
+      <h3 class="font-bold text-xl">What programming language should you start with?</h3>
+      <button class="px-4 py-2 font-bold mt-12 border border-black rounded-lg bg-yellow-400" @click="beginQuiz">Begin Quiz</button>
+    </div>
+    <div v-else>
+      <Question :text="questions[questionIndex].text"/>
+    </div>
   </div>
 </template>
 
 <script>
-import Question from './quiz-partials/Question.vue'
+import Question from './quiz-partials/Question.vue';
+import questionInfo from "../data/questions.json";
 
 export default {
   name: 'Quiz',
@@ -17,6 +23,15 @@ export default {
   data() {
     return {
       questionIndex: null,
+      questions: questionInfo.questions,
+    }
+  },
+  mounted() {
+    console.log(this.questions);
+  },
+  methods: {
+    beginQuiz() {
+      this.questionIndex = 0;
     }
   }
   
---
+++ src/components/quiz-partials/Answer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-for="(answer, index) in answers" :key="index">
+    <div :class="cardClass(index)">
+      <h3 class="font-bold text-lg">{{answer.text}}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Answer',
+  props: {
+    answers: Array,
+    questionIndex: Number
+  },
+  methods: {
+    cardClass(index) {
+      let baseClass = "px-4 py-2 mt-12 border border-black rounded-lg ";
+
+      if (index == 0) {
+        baseClass += "bg-blue-400"
+      } else if (index == 0) {
+        baseClass += "bg-red-400"
+      } else if (index == 0) {
+        baseClass += "bg-green-400"
+      } else {
+        baseClass += "bg-purple-400"
+      }
+
+      return baseClass;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
